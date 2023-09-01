d36eb8a67b8b5b6c9ef48b9f04a22abd7349b7d4
Erick Rodriguez <erickrod54@gmail.com>
2023-09-01 11:17:12 -0400

    server modifications

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "786840482bd6f2fc"; }
+/******/ 		__webpack_require__.h = function() { return "445f592d03ef469f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ pages/world.js
@@ -0,0 +1,7 @@
+const World = () => { 
+    return(
+        <h2>Hello - World</h2>
+    )
+}
+
+export default World;
