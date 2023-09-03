675d9d80e4b8329a91b0b1b56ccc96a68a326acd
Erick Rodriguez <erickrod54@gmail.com>
2023-09-03 04:34:34 -0400

    Discover-coffee-stores - version 1.02 - Banner - Features: Adding styles.buttonWrapper

--- components/banner.component.jsx
+++ components/banner.component.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import styles from '../styles/banner.module.css';
 
 /**
- * Discover-coffee-stores - version 1.01 -  Banner
+ * Discover-coffee-stores - version 1.02 -  Banner
  * - Fetaures:
  * 
- *    --> Building Banner Component 
+ *    --> Adding 'styles.buttonWrapper' 
  * 
  * 
  * Notes: here i receive the props and start tp apply it them
@@ -20,7 +20,9 @@ const Banner = (props) => {
                  <span className={styles.title2}> Connoisseur </span>
             </h1>
             <p className={styles.subTitle}>Discover your local coffe shops!</p>
-            <button className={styles.button} onClick={props.handleOnClick}>{props.buttonText}</button>
+            <div className={styles.buttonWrapper }>
+                <button className={styles.button} onClick={props.handleOnClick}>{props.buttonText}</button>
+            </div>
         </div>
     )
 }
