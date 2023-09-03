e1d86c8ab1e436cea6ccd20a46bb0a4644137b92
Erick Rodriguez <erickrod54@gmail.com>
2023-09-03 04:39:48 -0400

    Discover-coffee-stores - version 1.02 - DynamicRoute - Features: Building DynamicRoute Component

---
+++ pages/[dynamic].js
@@ -0,0 +1,34 @@
+import { useRouter } from "next/router"
+import Head from 'next/head';
+
+/**
+ * Discover-coffee-stores - version 1.02 -  DynamicRoute
+ * - Fetaures:
+ * 
+ *    --> Building 'DynamicRoute' Component
+ * 
+ * Note: By this route i can see how 'query' and 'router'
+ * can modify values of the 'Head' ( a behavior similar 
+ * to Helmet Component in React )
+ */
+
+const DynamicRoute = () => {
+
+    const router = useRouter();
+    const query = router.query.dynamic
+
+    console.log('value of the query => ', query)
+    /**as this log i know that the id match with 'dynamic' ( in 
+     * fact is my [dynamic] defined as the name of the file) */
+
+    return(
+        <div>
+            <Head>
+                <title>{query}</title>
+            </Head>
+            <div>Hi i am the dynamic route - {query}</div>
+        </div>
+    )
+}
+
+export default DynamicRoute
