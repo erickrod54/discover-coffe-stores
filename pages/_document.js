6fc8d6797ef651f540067150809368d19bf1ec4d
Erick Rodriguez <erickrod54@gmail.com>
2023-09-06 13:14:24 -0400

    Discover-coffee-stores - version 1.04 - MyDocument - Features: Adding IBMPlexSans-Bold.ttf font

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,11 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
+
 /**
- * Discover-coffee-stores - version 1.03 -  MyDocument
+ * Discover-coffee-stores - version 1.04 -  MyDocument
  * - Fetaures:
  * 
- *    --> Building 'MyDocument'
+ *    --> Adding 'IBMPlexSans-Bold.ttf' font 
  * 
  * Note: By building this page Document allows to change global
  * setting as the font and other properties applied to all the pages 
@@ -15,7 +16,9 @@ class MyDocument extends Document{
     render(){
         return(
             <Html lang="en">
-                <Head></Head>
+                <Head>
+                    <link rel='preload' href='/fonts/IBMPlexSans-Bold.ttf' as='font' crossOrigin='anonymous' /> 
+                </Head>
                 <body>
                     <Main></Main>
                         <NextScript />
