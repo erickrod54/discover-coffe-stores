a605c2904887678cf44a88000def34947806550e
Erick Rodriguez <erickrod54@gmail.com>
2023-09-07 12:04:43 -0400

    Server files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8e51841992cb0240"; }
+/******/ 		__webpack_require__.h = function() { return "db811f17efd961ab"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.57645b77b07b9429.hot-update.js
+++ .next/static/webpack/webpack.57645b77b07b9429.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c83be837a648f329"; }
+/******/ 	__webpack_require__.h = function() { return "09b821dc81714f19"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
