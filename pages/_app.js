b20c730e1979fb51705429f159d7254c48adb05a
Erick Rodriguez <erickrod54@gmail.com>
2023-09-09 02:09:09 -0400

    Discover-coffee-stores - version 1.12 - MyApp - Features: Disabling strict mode, Wrapping the app with the provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,27 @@
-import '../styles/globals.css'
+import { StrictMode } from 'react';
+import '../styles/globals.css';
+import { CoffeeStoresProvider } from '../context';
+
+/**
+ * Discover-coffee-stores - version 1.12 -  MyApp
+ * - Fetaures:
+ * 
+ *    --> Disabling strict mode.
+ * 
+ *    --> Wrapping the app with the provider. 
+ * 
+ * Note: By building this page Document allows to change global
+ * setting as the font and other properties applied to all the pages 
+ * 
+ */
 
 function MyApp({ Component, pageProps }) {
   return(
-    <>
-     <Component {...pageProps} />
-    </>
+    <StrictMode strict={false}>
+      <CoffeeStoresProvider>
+        <Component {...pageProps} />
+      </CoffeeStoresProvider>
+    </StrictMode>
   )
 }
 
