fccb8c165976996135fb2bc3f4cce19638458069
Erick Rodriguez <erickrod54@gmail.com>
2023-09-20 03:09:50 -0400

    Discover-coffee-stores -version 2.12 - context js - Features: Building ACTION_TYPES, Building storeReducer, Building initialState, Implementing useReducer, Providing coffeeStores, state, and dispatch

--- context.js
+++ context.js
@@ -1,25 +1,65 @@
-import React, { useContext } from "react";
+import React, { useContext, useReducer } from "react";
 import coffeeStores from './data/coffee-stores.json';
 
 
 /**
- * Discover-coffee-stores - version 1.13 -  context js
+ * Discover-coffee-stores - version 2.12 -  context js
  * - Fetaures:
  * 
- *    --> Importing and providing 'coffeeStores'
+ *    --> Building 'ACTION_TYPES' - for action names.
  * 
- * Note: This styles are grid layout based thinking in mobile
- * first
+ *    --> Building 'storeReducer' - to define action flow and set a payload
+ * 
+ *    --> Building 'initialState' - to manage multiple states and 'disptatch' mutator 
+ *        and providing them ( as LatLong, and coffeeStores )
+ * 
+ *    --> Implementing 'useReducer'
+ * 
+ *    --> Providing  'coffeeStores', 'state', 'dispatch'
+ *  
+ * Note: At this version the reducer and values can be checkcked by the browser
+ * in Components section
  */
 
 
 const CoffeeStoresContext = React.createContext();
 
+export const ACTION_TYPES = {
+    SET_LANG_LONG: 'SET_LANG_LONG',
+    SET_COFFEE_STORES: 'SET_COFFEE_STORES'
+}
+
+const storeReducer = (state, action ) => {
+    switch (action.type) {
+        case ACTION_TYPES.SET_LANG_LONG:{
+            return {...state, latLong: action.payload.latLong }
+        }
+        
+        case ACTION_TYPES.SET_COFFEE_STORES:{
+            return {...state, coffeeStores: action.payload.coffeeStores }  
+        }
+        
+        default:
+            throw new Error(`Unhandled action type: ${action.type}`)
+    }
+}
+
 export const CoffeeStoresProvider = ({ children }) => {
+
+    const initialState = {
+        latLong:"",
+        coffeeStores:[]
+    }
+
+    const [ state, dispatch ] = useReducer(storeReducer, initialState)
 
     return(
         <CoffeeStoresContext.Provider
-            value={{coffeeStores}}
+            value={{
+                coffeeStores,
+                state,
+                dispatch
+            }}
         >
             {children}
         </CoffeeStoresContext.Provider>
