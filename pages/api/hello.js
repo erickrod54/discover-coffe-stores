a6ab1cc3d77beb2d17473d9b2b28d5b9cbde3ea7
Erick Rodriguez <erickrod54@gmail.com>
2023-09-22 21:30:00 -0400

    Discover-coffee-stores -version 2.14 - hello js - Features: Testing hello js API values

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+/**
+ * Discover-coffee-stores - version 2.14 -  hello js
+ * - Fetaures:
+ * 
+ *    --> Testing hello js API values
+ * 
+ * Note: this page test can be modify later
+*/
 
-export default function handler(req, res) {
-  console.log({ req, res })
-  res.status(200).json({ name: 'John Doe' })
+
+export default (req, res) => {
+    res.status(200).json({ name: 'ER - For Erick Rodriguez'})
 }
