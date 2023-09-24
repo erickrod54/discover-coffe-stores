cc5fba4527c3da3c438022c386099b3c856d8338
Erick Rodriguez <erickrod54@gmail.com>
2023-09-23 21:30:00 -0400

    Discover-coffee-stores - version 2.15 - coffee-store-page - Features: Testing POST and GET flow - using postman -

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,19 +1,14 @@
 /**
- * Discover-coffee-stores - version 2.14 -  coffee-store-page
+ * Discover-coffee-stores - version 2.15 -  coffee-store-page
  * - Fetaures:
  * 
- *    --> Authenticanting with the airtable API
+ *    --> Testing 'POST' and 'GET' flow
  * 
- *    --> Building table and Testing in it.
+ * Note: the flow is being created with the 'if-else'
+ * condition, the result can be visualized using 
+ * 'postman' client - API tester -
  * 
- *    --> Building 'createCoffeeStore' and testing the API
- * 
- * Note: table will be apppear in the console as it is back end 
- * code, 
- *      and 
- * "Hi there" message will appear as result of the 
- * api url tested in "http://localhost:3000/api/createCoffeeStore"
- * in the browser
+ * by introducing 'http://localhost:3000/api/createCoffeeStore'
  */
 
 var Airtable = require('airtable');
@@ -24,7 +19,12 @@ const table = base('coffee-stores');
 console.log({ table })
 
 const createCoffeeStore = (req, res) => {
-    res.json({ message: "Hi there"})
+    console.log({ req })
+    if (req.method === 'POST') {
+        res.json({ message: "Hi there"});
+    } else {
+        res.json({ message: 'method is GET'});
+    }
 };
 
 export default createCoffeeStore;
