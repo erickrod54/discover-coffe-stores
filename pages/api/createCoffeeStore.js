b56aeab7a8fb246ad0687317ab7770c282b9213e
Erick Rodriguez <erickrod54@gmail.com>
2023-09-25 21:30:00 -0400

    Discover-coffee-stores - version 3.01 - coffe-store-page - Features: Building a flow that validate id to wrap find and create a coffee store

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,8 +1,9 @@
 /**
- * Discover-coffee-stores - version 3.00 -  coffee-store-page
+ * Discover-coffee-stores - version 3.01 -  coffee-store-page
  * - Fetaures:
  * 
- *    --> Validating 'id' and 'name' to 'createCoffee' Store
+ *    --> Building a flow that validate 'id' to wrap 'find'
+ *        and 'create' a coffee store
  * 
  * Note: this will prevent to create a new coffee store without
  * 'name' and 'id' that is essential in order to identify them
@@ -24,53 +25,57 @@ const createCoffeeStore = async (req, res) => {
         //find a record
     
         const { id, name, address, dma, vote, imgUrl } = req.body;
-    
-        const findingCoffeeStoreRecords = await table.select({
-            filterByFormula: `id=${id}`
-        }).firstPage();
-    
-        console.log({ findingCoffeeStoreRecords })
-    
-        try {
-            if (findingCoffeeStoreRecords.length !== 0) {
-                const records = findingCoffeeStoreRecords.map((records) => {
-                    return{
-                        ...records.fields
-                    }
-                })
-                res.json(records)
-            }else{
-                if (name && id) {
-                    //create a record
-                        const createRecords = await table.create([
-                            {
-                                fields: {
-                                    id,
-                                    name,
-                                    address,
-                                    dma,
-                                    vote,
-                                    imgUrl
+
+        if (id) {
+            
+            const findingCoffeeStoreRecords = await table.select({
+                filterByFormula: `id=${id}`
+            }).firstPage();
+        
+            console.log({ findingCoffeeStoreRecords })
+        
+            try {
+                if (findingCoffeeStoreRecords.length !== 0) {
+                    const records = findingCoffeeStoreRecords.map((records) => {
+                        return{
+                            ...records.fields
+                        }
+                    })
+                    res.json(records)
+                }else{
+                    if (name) {
+                        //create a record
+                            const createRecords = await table.create([
+                                {
+                                    fields: {
+                                        id,
+                                        name,
+                                        address,
+                                        dma,
+                                        vote,
+                                        imgUrl
+                                    }
                                 }
-                            }
-                        ]) 
-                        const records = createRecords.map((records) => {
-                            return{
-                                ...records.fields
-                            }
-                        })
-                        res.json({records})
-                    }else{
-                        res.status(400)
-                        res.json({ message: 'Id or name is missing'})
+                            ]) 
+                            const records = createRecords.map((records) => {
+                                return{
+                                    ...records.fields
+                                }
+                            })
+                            res.json({records})
+                        }else{
+                            res.status(400)
+                            res.json({ message: 'Id or name is missing'})
+                        }
                     }
-                }
-        } catch (err){
-            console.log('Error finding store', err );
-            res.status(500);
-            res.json({ message: 'Error finding store', err});
-        } 
-    };
-    }
+            } catch (err){
+                console.log('Error finding store', err );
+                res.status(500);
+                res.json({ message: 'Error finding store', err});
+            } 
+        };
+        }
+        }
+    
 
 export default createCoffeeStore;
