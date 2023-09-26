53d46359a972ecc0f54966f9f80a3c8bb1111df1
Erick Rodriguez <erickrod54@gmail.com>
2023-09-25 21:30:00 -0400

    Discover-coffee-stores - version 2.24 - coffeestore-page - Features: Wrapping everything with the POST flow, Making createRecords dynamic

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,14 +1,15 @@
 /**
- * Discover-coffee-stores - version 2.22 -  coffee-store-page
+ * Discover-coffee-stores - version 2.24 -  coffee-store-page
  * - Fetaures:
  * 
- *    --> Clearing 'createRecords' message
+ *    --> Wrapping everything with the 'POST' flow.
  * 
- *    --> Changing id to check id creation.
+ *    --> Making createRecords dynamic.
  * 
- * Note: create record code block is being modifying to create the
- * coffee store record - harcoded id is set as 3 temproraly so 
- * i can get the message "create a record"
+ * Note: createRecors is made dynamic by destructuring field 
+ * directly from the 'req.body' from the 'airtables'
+ * 
+ * 
  */
 
 var Airtable = require('airtable');
@@ -20,48 +21,54 @@ console.log({ table })
 
 const createCoffeeStore = async (req, res) => {
     console.log({ table })
-    //find a record
 
-    const findingCoffeeStoreRecords = await table.select({
-        filterByFormula:`id="3"`
-    }).firstPage();
+    if (req.method === "POST") {
 
-    console.log({ findingCoffeeStoreRecords })
-
-    try {
-        if (findingCoffeeStoreRecords.length !== 0) {
-            const records = findingCoffeeStoreRecords.map((records) => {
-                return{
-                    ...records.fields
-                }
-            })
-            res.json(records)
-        }else{
-        //create a record
-            const createRecords = await table.create([
-                {
-                    fields: {
-                        id: "3",
-                        name: "Favorite Coffee Store Erick with discount",
-                        address: "patua vida mia",
-                        dma: "patua blv",
-                        vote: 200,
-                        imgUrl: "http://myimgg.com",
+        //find a record
+    
+        const { id, name, address, dma, vote, imgUrl } = req.body;
+    
+        const findingCoffeeStoreRecords = await table.select({
+            filterByFormula: `id=${id}`
+        }).firstPage();
+    
+        console.log({ findingCoffeeStoreRecords })
+    
+        try {
+            if (findingCoffeeStoreRecords.length !== 0) {
+                const records = findingCoffeeStoreRecords.map((records) => {
+                    return{
+                        ...records.fields
                     }
-                }
-            ]) 
-            const records = createRecords.map((records) => {
-                return{
-                    ...records.fields
-                }
-            })
-            res.json({records})
-        }
-    } catch (err){
-        console.log('Error finding store', err );
-        res.status(500);
-        res.json({ message: 'Error finding store', err});
-    } 
-};
+                })
+                res.json(records)
+            }else{
+            //create a record
+                const createRecords = await table.create([
+                    {
+                        fields: {
+                            id,
+                            name,
+                            address,
+                            dma,
+                            vote,
+                            imgUrl
+                        }
+                    }
+                ]) 
+                const records = createRecords.map((records) => {
+                    return{
+                        ...records.fields
+                    }
+                })
+                res.json({records})
+            }
+        } catch (err){
+            console.log('Error finding store', err );
+            res.status(500);
+            res.json({ message: 'Error finding store', err});
+        } 
+    };
+    }
 
 export default createCoffeeStore;
