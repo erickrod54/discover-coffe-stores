90a201c3f9bdf0909a5763c21d8c9ce384a9bf5d
Erick Rodriguez <erickrod54@gmail.com>
2023-09-27 21:30:00 -0400

    Discover-coffee-stores - version 3.07 - MyApp - Features: Clearing StrictMode import

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
-import { StrictMode } from 'react';
 import '../styles/globals.css';
 import { CoffeeStoresProvider } from '../context';
 
 /**
- * Discover-coffee-stores - version 3.06 -  MyApp
+ * Discover-coffee-stores - version 3.07 -  MyApp
  * - Fetaures:
  * 
- *    --> Taking 'StrictMode' off and switching it from
- *        'next.config.js' 
+ *    --> Clearing 'StrictMode' import 
  * 
  * Note: By building this page Document allows to change global
  * setting as the font and other properties applied to all the pages 
