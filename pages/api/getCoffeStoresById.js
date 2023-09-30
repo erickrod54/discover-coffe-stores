873f394f9fd3ce4172408dbc34308a5f121a8a40
Erick Rodriguez <erickrod54@gmail.com>
2023-09-29 21:30:00 -0400

    Discover-coffee-stores - version 3.12 - getCoffeeStoreById - Features: Testing API without the id

--- pages/api/getCoffeStoresById.js
+++ pages/api/getCoffeStoresById.js
@@ -1,34 +1,23 @@
 /**
- * Discover-coffee-stores - version 3.11 -  getCoffeeStoreById
+ * Discover-coffee-stores - version 3.12 -  getCoffeeStoreById
  * - Fetaures:
  * 
- *    --> Building 'getCoffeeStoreById'.
+ *    --> Testing API without the 'id'
  * 
- *    --> Testing 'id' using 'postman'  
- * 
- * Note: the test consists in a sample id from the 'airtable'
- * store data as:
- * 
- *      '543e744b498e88f9b19fccfc'
- * 
- * then i build this 'file' and the 'api name' as:
- * 
- *  http://localhost:3000/api/getCoffeStoresById?id=543e744b498e88f9b19fccfc
- * 
- * then i send it and i should visualize in 'postman' the message:
- * 
- *  {
- * 
- *  "message": "id is created 543e744b498e88f9b19fccfc"
- *   
- *  }
+ * Note: the test is for 'res.status(400)' that 
+ * is part od the 'id' flow
  */
 
 const getCoffeeStoreById = (req, res) => {
     const { id } = req.query;
 
     try {
-        res.json({ message: `id is created ${id}`})
+        if (id) {
+            res.json({ message: `id is created ${id}`})
+        }else{
+            res.status(400);
+            res.json({ message: "Id is missing"})
+        }
         
     } catch (error) {
         res.status(500)
