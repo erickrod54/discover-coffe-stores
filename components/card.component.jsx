839500a65d669bbbd60f8a93c079ed74db18bce2
Erick Rodriguez <erickrod54@gmail.com>
2023-10-06 21:30:00 -0400

    Discover-coffee-stores - version 4.07 - Card - Features: Adding props.name to Image

--- components/card.component.jsx
+++ components/card.component.jsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 import styles from '../styles/card.module.css';
 import cls from 'classnames'
 
-/* Dicover-coffee-stores - version 1.07 - Card - 
+/* Dicover-coffee-stores - version 4.07 - Card - 
 * Features: 
 *
 *
-*    --> Building and styling 'Card'.
+*    --> Adding 'alt={props.name}' to Image.
 *
 *Note: this is the initial stylesheet 
 *
@@ -24,7 +24,7 @@ const Card = (props) => {
                         <h2 className={styles.cardHeader}>{props.name}</h2>
                     </div>
                     <div className={styles.cardImageWrapper}>
-                        <Image className={styles.cardImage} src={props.imgUrl} width={260} height={160}/>
+                        <Image alt={props.name} className={styles.cardImage} src={props.imgUrl} width={260} height={160}/>
                     </div>
                 </div>
             </a>
